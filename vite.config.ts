011648ef7d507b3307a18652af856e72f502f615
enikodmolnar <eniko.d.molnar@gmail.com>
2026-05-06 10:59:22 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/swift-home-deal/"
   server: {
     host: "::",
     port: 8080,
